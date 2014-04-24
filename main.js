d2c5b89fc89a0825771435159b3f3f32cbf40f54
Jennifer Fong <jen@ednapiranha.com>
2014-04-24 14:57:38 -0400

    send text in to return possible match w/ tests

--- main.js
+++ main.js
@@ -1,1 +1,51 @@
 'use strict';
+
+var Psychonaut = function (options) {
+  var natural = require('natural');
+  var BloomFilter = require('bloomfilter').BloomFilter;
+
+  natural.PorterStemmer.attach();
+
+  var bloom = new BloomFilter(
+    32 * 256, // number of bits to allocate.
+    16        // number of hash functions.
+  );
+
+  if (!options || (!options.content && typeof options.content !== 'object')) {
+    throw new Error('You need content to match keywords from');
+  }
+
+  var getSuggested = function (word) {
+    return options.content[word] || false;
+  };
+
+  var add = function (stemmed) {
+    stemmed.forEach(function (word) {
+      if (options.content[word]) {
+        bloom.add(word);
+      }
+    });
+  };
+
+  this.tag = function (text) {
+    var stemmed = text.tokenizeAndStem();
+
+    add(stemmed);
+
+    return stemmed;
+  };
+
+  this.check = function (text) {
+    var stemmed = this.tag(text);
+
+    for (var i = 0; i < stemmed.length; i ++) {
+      if (bloom.test(stemmed[i])) {
+        return getSuggested(stemmed[i]);
+      }
+    }
+
+    return false;
+  };
+};
+
+module.exports = Psychonaut;
--- test/test.main.js
+++ test/test.main.js
@@ -3,3 +3,39 @@
 process.env.NODE_ENV = 'test';
 
 var should = require('should');
+var Psychonaut = require('../main');
+
+var ads = {
+  'hike': ['REI', 'MEC'],
+  'music': ['soundcloud', 'youtube']
+};
+
+var p = new Psychonaut({
+  content: ads
+});
+
+describe('psychonaut', function () {
+  describe('tokenize', function () {
+    it('should tokenize and stem a sentence containing hike and return hike ads', function () {
+      var s = 'I am hiking today at noon';
+      p.tag(s);
+      p.check(s).should.eql(ads.hike);
+    });
+
+    it('should tokenize and stem a sentence containing music and return music ads', function () {
+      var s = 'I am listening to music today at noon';
+      p.tag(s);
+      p.check(s).should.eql(ads.music);
+    });
+
+    it('should tokenize and stem a sentence containing hike and music and return hike ads', function () {
+      var s = 'I am hiking today at noon and listening to music';
+      p.tag(s);
+      p.check(s).should.eql(ads.hike);
+    });
+
+    it('should not return hike or music ads', function () {
+      p.check('I am just hanging out, no boggie').should.eql(false);
+    });
+  });
+});
